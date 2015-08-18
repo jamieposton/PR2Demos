28fc3385eeb49a2bf97b57a41763f02259dbd33c
standupsesame <tigerbounces@gmail.com>
2015-08-18 11:08:56 -0700

    Added all files

---
+++ build/CMakeFiles/mac.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mac.dir/src/mac.cpp.o"
+  "../bin/mac.pdb"
+  "../bin/mac"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
